b01cc6d3e1611f5005947f4cc8cf72711e935292
Christophe Savard <christophe_savard@hotmail.ca>
2016-05-05 03:15:52 -0400

    Add Bash solution to Horse-racing Duals

---
+++ Solutions/Easy/Horse-racing Duals/Program.bash
@@ -0,0 +1,50 @@
+read N              #Amount of horses
+
+declare -a horses   #Array to store the horses
+
+
+
+#Reading all values
+
+for (( i=0; i<N; i++ )); do
+
+    read h
+
+    horses[i]=$h
+
+done
+
+
+
+#Sorting
+
+horses=($(for i in "${horses[@]}"; do echo $i; done | sort -n))
+
+
+
+smallest=$((${horses[1]} - ${horses[0]}))   #Set smallest diff to the diff of the two first horses
+
+current=${horses[1]}                        #Current is second horse
+
+
+
+#Loop through the remaining horses
+
+for next in "${horses[@]:2}"; do
+
+    diff=$(($next - $current))
+
+    if [ $diff -lt $smallest ]; then
+
+        smallest=$diff
+
+    fi
+
+    current=$next
+
+done
+
+
+
+#Write smallest
+echo $smallest
